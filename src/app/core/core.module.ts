dd9b431ddecd930f8547019f6df67edb5be1f502
Aleksandra Culinovic <aleksandraculinovic@gmail.com>
2021-01-28 19:03:10 +0100

    created service, interface and pipe files

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { SearchPipe } from './pipes/search.pipe';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, SearchPipe],
   imports: [
     CommonModule,
     CoreRoutingModule
