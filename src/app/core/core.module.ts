bfdb3bd1e9aad58714c701f7988076c51370fde2
Aleksandra Culinovic <aleksandraculinovic@gmail.com>
2021-01-28 19:08:59 +0100

    added layout and router-outlet

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 
+// ROUTING
 import { CoreRoutingModule } from './core-routing.module';
+
+// PIPE
 import { SearchPipe } from './pipes/search.pipe';
+
+// ANGULAR MATERIAL
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [LayoutComponent, SearchPipe],
   imports: [
     CommonModule,
-    CoreRoutingModule
-  ]
+    CoreRoutingModule,
+    MatToolbarModule
+  ],
+  exports: [LayoutComponent]
 })
 export class CoreModule { }
